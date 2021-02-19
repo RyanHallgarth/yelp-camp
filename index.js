1821346dc14deccabd54dbd34831ee551ba56e13
rhallgarth88 <59847530+rhallgarth88@users.noreply.github.com>
2021-02-19 18:26:58 -0500

    Implemented flash-connect node package to push 'success' and 'error' text to the UI

--- index.js
+++ index.js
@@ -3,12 +3,13 @@ const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
 const ejsMate = require('ejs-mate');
+const session = require('express-session');
+const flash = require('connect-flash')
 const ExpressError = require('./utils/ExpressError');
 const methodOverride = require('method-override');
-const campgrounds = require('./routes/campgroundRoutes');
-const reviews = require('./routes/reviewRoutes')
 
-const app = express();
+const campgrounds = require('./routes/campgroundRoutes');
+const reviews = require('./routes/reviewRoutes');
 
 mongoose.connect('mongodb://localhost:27017/yelp-camp', {
     useNewUrlParser: true,
@@ -23,12 +24,34 @@ db.once("open", () => {
     console.log("Database connected");
 });
 
+const app = express();
+
 app.engine('ejs', ejsMate);
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'))
 
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
+app.use(express.static(path.join(__dirname, 'public')));
+
+const sessionConfig = {
+    secret: 'thisshouldbeabettersecret',
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+        HttpOnly: true,
+        expires: Date.now() + 604800000,
+        maxAge: 604800000
+    }
+}
+app.use(session(sessionConfig));
+app.use(flash());
+
+app.use((req, res, next) => {
+    res.locals.success = req.flash('success');
+    res.locals.error = req.flash('error');
+    next();
+})
 
 
 app.use('/campgrounds', campgrounds);
