f72b817738096e90ae70e53286d02a3485b4705d
rhallgarth88 <59847530+rhallgarth88@users.noreply.github.com>
2021-02-20 15:54:23 -0500

    Added 'star' rating system for reviews

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -6,14 +6,16 @@ const User = require('../models/user')
 const users = require('../controllers/userCon')
 
 
-router.get('/register', users.renderRegister)
-
-router.post('/register', catchAsync(users.register))
 
 
-router.get('/login', users.renderLogin)
+router.route('/register')
+    .get(users.renderRegister)
+    .post(catchAsync(users.register));
 
-router.post('/login', passport.authenticate('local', { failureFlash: true, failureRedirect: '/login' }), users.login)
+router.route('/login')
+    .get(users.renderLogin)
+    .post(passport.authenticate('local', { failureFlash: true, failureRedirect: '/login' }), users.login);
+
 
 router.get('/logout', users.logout)
 
