48a3a7c302c4191a78fba19220535959fd9a1531
rhallgarth88 <59847530+rhallgarth88@users.noreply.github.com>
2021-02-22 20:24:22 -0500

    try to deploy to heroku

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ const reviewRoutes = require('./routes/reviewRoutes');
 
 const MongoDBStore = require("connect-mongo")(session);
 
-const dbUrl = 'mongodb://localhost:27017/yelp-camp';
+const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/yelp-camp';
 
 mongoose.connect(dbUrl, {
     useNewUrlParser: true,
